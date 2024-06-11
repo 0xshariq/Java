8856bf45717b9e369429f0f51f028548586a1b70
Shariq Choudhary <145042629+0xshariq@users.noreply.github.com>
2024-06-11 13:45:34 +0000

    Upload two basic codes

---
+++ Input.java
@@ -0,0 +1,9 @@
+// Print integer value 
+public class Input {
+  public static int input(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(n);
+    return 0;
+  }
+}
