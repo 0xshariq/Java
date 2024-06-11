8d3acda7b836d41f9b426e18ce21356fbb7b338d
Shariq Choudhary <145042629+0xshariq@users.noreply.github.com>
2024-06-11 14:25:30 +0000

    Upload two codes

---
+++ Sub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sub{
+  public static int sub(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int minus = a - b;
+    System.out.println(minus);
+    return 0;
+  }
+}
---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sum{
+  public static int sum(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int add = a + b;
+    System.out.println(add);
+    return 0;
+  }
+}
